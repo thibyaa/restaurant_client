754929ecbdb920e1ba4fbea619085c9878f507ce
Thibyaa <thibyaa@gmail.com>
2024-03-02 17:30:38 +0000

    cors error fixed, additional script added

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
 
   const fetchData = async () => {
     const response = await fetch(
-     "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF");
+     "http://localhost:8010/proxy/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF");
      const jsonData = await response.json();
      console.log(jsonData);
-     setRestaurantData(jsonData);
   }
 
   fetchData();
