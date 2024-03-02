fdfa01060049fc8dfb56b61adddfb1fbe1e5ff28
Thibyaa Mahasivam <thibyaa@gmail.com>
2024-03-02 16:59:27 +0000

    env created + fetch request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import CardContainer from'./containers/CardContainer';
 
 function App() {
+
+  const [restaurantData, setRestaurantData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+     "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF");
+     const jsonData = await response.json();
+     console.log(jsonData);
+     setRestaurantData(jsonData);
+  }
+
+  fetchData();
 
   return (
   <>
