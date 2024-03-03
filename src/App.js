30c7a98094077b25d86eeb8b27bb915400b137f1
Thibyaa <thibyaa@gmail.com>
2024-03-03 22:02:13 +0000

    react-router library imported

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "./App.css";
 import CardContainer from "./containers/CardContainer";
